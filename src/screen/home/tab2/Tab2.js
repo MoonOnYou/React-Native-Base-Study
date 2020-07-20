3f1650d6cf796a7fac8893cfa9c08c8f96f8027b
onyou moon <onyoumoon@onyouui-MacBookPro.local>
2020-07-20 14:23:50 +0900

    KeyboardAvoidingView 써봄.. 근데 .. 내 예상과는 다르게 동작하는듯 ..

--- src/screen/home/tab2/Tab2.js
+++ src/screen/home/tab2/Tab2.js
@@ -1,10 +1,47 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  Platform,
+  TouchableWithoutFeedback,
+  KeyboardAvoidingView,
+  TextInput,
+  Keyboard,
+} from 'react-native';
 
 export default function Tab2() {
   return (
-    <View>
-      <Text>tab2</Text>
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
+      style={styles.container}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.inner}>
+          <TextInput placeholder="Username" style={styles.textInput} />
+          <Button title="Submit" onPress={() => null} />
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  inner: {
+    padding: 24,
+    flex: 1,
+    justifyContent: 'space-around',
+  },
+  header: {
+    fontSize: 52,
+    marginBottom: 120,
+  },
+  textInput: {
+    height: 40,
+    borderColor: '#000000',
+    borderBottomWidth: 1,
+  },
+});
